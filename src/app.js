c67dd173f80dd8cf2b870c33d1edc94b1d24cfd3
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-18 09:27:20 +0100

    properties

--- src/app.js
+++ src/app.js
@@ -5,8 +5,17 @@ import Header from './components/header/header'
 function App() {
   return (
     <>
-    <Header />
-    <Header />
+    <Header title="title one">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </Header>
+    <Header title="title two">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul></Header>
     </>
   );
 }
