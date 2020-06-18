dcce6396476696f18acda79f4ee180ee1f0ecea6
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-18 12:14:41 +0100

    Listing datas from API and axius

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 import Header from './components/header/header'
 
 function App() {
-  const [projects, setProjects] = useState(['Apps development', 'Web Frontend']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setProjects(res.data);
+    });
+  }, []);
 
   function handleAddProject() {
-    // projects.push();
-
     setProjects([ ... projects, `New Project ${Date.now()}`]);
-
-    console.log(projects);
   }
 
   return (
     <>
       <Header title="Projects" />
 
-      <img src={backgroundImage} width={300} alt=""/>
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Add Project</button>
