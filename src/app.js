d066de867c7deb561fd71bcf630241300f419386
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-18 13:28:03 +0100

    Registring projects

--- src/app.js
+++ src/app.js
@@ -14,8 +14,19 @@ function App() {
     });
   }, []);
 
-  function handleAddProject() {
-    setProjects([ ... projects, `New Project ${Date.now()}`]);
+  async function handleAddProject() {
+    // setProjects([ ... projects, ]);
+
+    const res = await api.post('repositories', {
+        title: `New Project ${Date.now()}`, 
+        url: `challenge-nodejs-and-expressjs`,
+        techs: `['nodejs','reactjs']`
+    });
+
+    const project = res.data;
+
+    setProjects([ ... projects, project ]);
+
   }
 
   return (
@@ -23,7 +34,7 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+  {projects.map(project => <li key={project.id}>{project.title}; {project.url}; {project.techs}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Add Project</button>
